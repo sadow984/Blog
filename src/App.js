fc7fa5e229150562d2dce854e28fa811e543b7f5
Test.User1 <Test.User1@TSCDELLAP304.thesmartcube.net>
2020-02-13 22:35:09 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "./Header/Header";
-import './App.css';
+import "./App.css";
 
-class App extends Component{
+class App extends Component {
   render() {
-    
-
-
-    return (
-      <Header></Header>
-    );
+    return <Header />;
   }
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react";
 import classes from "./Header.css";
 import { MdSearch } from "react-icons/md";
 const header = props => {
-    
-
-    return (
-      <div className="Header">
-        <div className="Logo">
-          <h1>Blog</h1>
+  return (
+    <div className="Header">
+      <div className="Logo">
+        <h1>Blog</h1>
+      </div>
+      <div className="Search">
+        <div className="Bar">
+          <input type="text" className="InBar" value="Posts"></input>
         </div>
-        <div className="Search">
-          <div className="Bar">
-            <input type="text" value="Posts"></input>
-          </div>
-          <div className="SearchIcon">
-            <MdSearch></MdSearch>
-          </div>
-        </div>
-        <div className="Author">
-          <h1>Author</h1>
+        <div className="SearchIcon">
+          <button type="submit" class="searchButton">
+            <i>
+              <MdSearch></MdSearch>
+            </i>
+          </button>
         </div>
       </div>
-    );
-}
+      <div className="Author">
+        <h4>Author</h4>
+      </div>
+    </div>
+  );
+};
 
 export default header;
