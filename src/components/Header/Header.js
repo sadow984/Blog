6eab8f7927d9f5676712934556e22535881172ea
Sanjeev <sanjeevsharma2552@gmail.com>
2020-02-16 16:06:51 +0530

    Header almost complete few things need to add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from 'react';
+import { Button, IconButton, Input } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
+import classes from './Header.module.css';
+
+const Header = () => {
+  const [searchVisible, setSearchVisibleHandler] = useState(false);
+  const inputRef = useRef(null);
+
+  const changeSearchState = () => {
+    setSearchVisibleHandler(!searchVisible);
+    inputRef.current.value = '';
+  };
+
+  return (
+    <header className={classes.Header}>
+      <div className={classes.Logo}>
+        <div className={classes.LogoDiv}>
+          <span>BG</span>
+        </div>
+      </div>
+      <section className={classes.section}>
+        <div>
+          <IconButton
+            title='Search Blog'
+            onClick={changeSearchState}
+            disableRipple='true'
+            disableFocusRipple='true'
+            size='small'
+          >
+            <Search className={classes.IconClass} />
+          </IconButton>
+          <span
+            style={{ display: `${searchVisible ? 'inline-block' : 'none'}` }}
+          >
+            <Input
+              ref={inputRef}
+              autoFocus={true}
+              disableUnderline={true}
+              placeholder='Search Blog'
+            />
+          </span>
+        </div>
+      </section>
+      <div style={{ marginRight: '1rem' }}>
+        <Button
+          disableRipple='true'
+          variant='contained'
+          color='primary'
+          size='medium'
+        >
+          LOGIN
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
