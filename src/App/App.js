c8d7ba5ec4b49ef46b5dd0208f8775c05833c15c
Test.User1 <Test.User1@TSCDELLAP304.thesmartcube.net>
2020-02-18 23:44:38 +0530

    Footer added

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from '../components/Header/Header';
 import Cards from '../components/card/cards/Cards';
+import Footer from '../components/Footer/Footer'
 
 const App = () => (
   <div className='App'>
     <Header />
-    <Cards/>
+    <Cards />
+    <Footer/>
   </div>
 );
 
