216fcca9231cdd22b2cfe9c35e4cc96e1f6f65ea
rajnis09 <rajnish.kumar2998@gmail.com>
2020-02-20 14:06:42 +0530

    fixed footer

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-import './App.css';
-import Header from '../components/Header/Header';
-import Container from '../components/card/container/Container';
-import Footer from '../components/Footer/Footer'
+import "./App.css";
+import Header from "../components/Header/Header";
+import Container from "../components/card/container/Container";
+import Footer from "../components/Footer/Footer";
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <Container />
-    <Footer />
+  <div>
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
+    <div>
+      <Footer />
+    </div>
   </div>
 );
 
