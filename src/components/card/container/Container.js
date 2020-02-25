9b2458136e69009b3329218451c84c3ec28c5517
Test.User1 <Test.User1@TSCDELLAP304.thesmartcube.net>
2020-02-25 21:44:52 +0530

    none

--- src/components/card/container/Container.js
+++ src/components/card/container/Container.js
@@ -3,17 +3,6 @@ import Cards from "../cards/Cards";
 import classes from "./Container.module.css";
 
 const Container = () => {
-<<<<<<< HEAD
-    
-
-    return (
-      <div>
-        <span>Recent</span>
-        <Cards />
-        <Cards />
-        <span>Popular</span>
-        <Cards />
-=======
   return (
     <div className={classes.ContainerWrapper}>
       <span className={classes.CategoryName}>Recent</span>
@@ -30,7 +19,6 @@ const Container = () => {
       </div>
       <span className={classes.CategoryName}>Popular</span>
       <div className={classes.Popular}>
->>>>>>> 4a5b1184050b9971f6891cdcad34e17f65600fd9
         <Cards />
       </div>
     </div>
