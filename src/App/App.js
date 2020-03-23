71008f28765ee27fd9b6309647baa3fff85455ea
rajnis09 <rajnish.kumar2998@gmail.com>
2020-03-23 16:50:54 +0530

    fixed footer

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Routers/Router";
 
 const App = () => (
   <BrowserRouter>
-    <div className="App">
+    <div>
       <Router />
     </div>
   </BrowserRouter>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,35 +3,31 @@ import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={classes.FooterWrapper}>
-      <footer className={classes.MainFooter}>
-        <div className={classes.Logo}>
-          <div className={classes.LogoDiv}>
-            <span>BG</span>
-          </div>
-        </div>
-        <nav>
-          <ul className={classes.MainFooterLinks}>
-            <li className={classes.MainFooterLink}>
-              <a href="#">Privacy</a>
-            </li>
-            <li className={classes.MainFooterLink}>
-              <a href="#">About</a>
-            </li>
-            <li className={classes.MainFooterLink}>
-              <a href="#">Help</a>
-            </li>
-            <li className={classes.MainFooterLink}>
-              <a href="#">Support</a>
-            </li>
-            <li className={classes.MainFooterLink}>
-              <a href="#">Terms of Use</a>
-            </li>
-          </ul>
-        </nav>
-      </footer>
-    </div>
-  );
+    <footer className={classes.MainFooter}>
+      <div className={classes.LogoWrapper}>
+        <span>BG</span>
+      </div>
+      <nav>
+        <ul className={classes.MainFooterLinks}>
+          <li className={classes.MainFooterLink}>
+            <a href="/">Privacy</a>
+          </li>
+          <li className={classes.MainFooterLink}>
+            <a href="/">About</a>
+          </li>
+          <li className={classes.MainFooterLink}>
+            <a href="/">Help</a>
+          </li>
+          <li className={classes.MainFooterLink}>
+            <a href="/">Support</a>
+          </li>
+          <li className={classes.MainFooterLink}>
+            <a href="/">Terms of Use</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+);
 };
 
 export default Footer;
