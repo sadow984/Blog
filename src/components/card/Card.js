c21c809a145163c5fd2833ae8e5768405de2cf2f
rajnis09 <rajnish.kumar2998@gmail.com>
2020-03-25 14:28:37 +0530

    added new card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,33 +7,49 @@ import { Link } from "react-router-dom";
 const Card = props => {
   return (
     <div className={classes.CardContainer}>
-      <Link
+      <Link className={classes.Link}
         to={{
-          pathname: "/blog", title: props.title, date: props.date,
-          data: props.data, count: props.counts, clap: props.clap
-        }}>
-        <img
-          src={props.image}
-          className={classes.ImageContainer}
-          onClick={props.clicked}
-        ></img>
+          pathname: "/blog",
+          title: props.title,
+          date: props.date,
+          data: props.data,
+          count: props.counts,
+          clap: props.clap
+        }}
+      >
+        <div className={classes.CardBody}>
+          <div
+            className={classes.CardImage}
+            style={{ backgroundImage: `url(${props.image})` }}
+          ></div>
+          <div className={classes.CardInfo}>
+            <div className={classes.CardInfoDetails}>
+              <span className={classes.CardCategory}> Development</span>
+              <h3 className={classes.CardTitle}>{props.title}</h3>
+              <span className={classes.CardBy}>
+                by
+                <a href="#" className={classes.CardAuthor} title="author">
+                  Jhon Doe
+                </a>{" "}
+                on {props.date}
+              </span>
+            </div>
+            <div className={classes.LikeButton}>
+              <IconButton
+                onClick={props.clap}
+                color="secondary"
+                disableRipple="true"
+                disableFocusRipple="true"
+                size="small"
+                edge="end"
+              >
+                <FavoriteIcon />
+                {props.counts}
+              </IconButton>
+            </div>
+          </div>
+        </div>
       </Link>
-      <div className={classes.CardBody}>
-        {/* <div className={classes.Author}></div> */}
-        <h2>{props.title}</h2>
-        <p>{props.date}</p>
-        <IconButton
-          onClick={props.clap}
-          color="secondary"
-          disableRipple="true"
-          disableFocusRipple="true"
-          size="small"
-          edge="end"
-        >
-          <FavoriteIcon />
-          {props.counts}
-        </IconButton>
-      </div>
     </div>
   );
 };
